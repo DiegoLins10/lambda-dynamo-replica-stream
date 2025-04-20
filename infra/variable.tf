1a8120ab28c576ecc85e819560e688f67f403aef
DiegoLins10 <diegofernandeslins@gmail.com>
2025-04-20 01:57:34 -0300

    dynamodb_stream_arn

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,9 +1,3 @@
 variable "lambda_function_name" {
   default = "lambda-dynamo-replica-stream"
 }
-
-variable "dynamodb_stream_arn" {
-  description = "ARN do stream do DynamoDB"
-  type        = string
-  sensitive   = true
-}
